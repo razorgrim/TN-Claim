50a394576f145ddf9b0f0cc6294b580c00668d5b
razorgrim <wazifjipang@gmail.com>
2026-06-19 09:11:09 +0800

    js

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: ['claim.neutron-tech.xyz'],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
@@ -27,6 +28,7 @@ export default defineConfig({
   preview: {
     host: true,
     port: 5173,
+    allowedHosts: ['claim.neutron-tech.xyz'],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
